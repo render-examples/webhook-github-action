ac15222619811964836df3db829a988c8c247be7
Scott Numamoto <scottnuma@users.noreply.github.com>
2025-02-24 11:26:48 -0800

    Update

--- app.ts
+++ app.ts
@@ -7,14 +7,30 @@ const app = express();
 const port = process.env.PORT || 3001;
 const renderWebhookSecret = process.env.RENDER_WEBHOOK_SECRET || '';
 
+if (!renderWebhookSecret ) {
+    console.error("Error: RENDER_WEBHOOK_SECRET is not set.");
+    process.exit(1);
+}
+
 const renderAPIURL = process.env.RENDER_API_URL || "https://api.render.com/v1"
 
 // To create a Render API token, follow instructions here: https://render.com/docs/api#1-create-an-api-key
-const renderAPIToken = process.env.RENDER_API_TOKEN || '';
+const renderAPIToken = process.env.RENDER_API_KEY || '';
+
+if (!renderAPIToken) {
+    console.error("Error: RENDER_API_KEY is not set.");
+    process.exit(1);
+}
 
 const githubAPIToken = process.env.GITHUB_API_TOKEN || '';
 const githubOwnerName = process.env.GITHUB_OWNER_NAME || '';
 const githubRepoName = process.env.GITHUB_REPO_NAME || '';
+
+if (!githubAPIToken || !githubOwnerName || !githubRepoName) {
+		console.error("Error: GITHUB_API_TOKEN, GITHUB_OWNER_NAME, or GITHUB_REPO_NAME is not set.");
+		process.exit(1);
+}
+
 const githubWorkflowID = process.env.GITHUB_WORKFLOW_ID || 'example.yaml';
 
 const octokit = new Octokit({
@@ -45,6 +61,10 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     }
 });
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Render Webhook GitHub Action is listening!')
+})
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 function validateWebhook(req: Request) {
@@ -62,6 +82,7 @@ async function handleWebhook(payload: WebhookPayload) {
     try {
         switch (payload.type) {
             case "deploy_ended":
+                console.log("handling deploy_ended event")
                 const event = await fetchEventInfo(payload)
 
                 // TODO add human readable status
@@ -84,7 +105,7 @@ async function handleWebhook(payload: WebhookPayload) {
                 }
 
                 console.log(`triggering github workflow for ${githubOwnerName}/${githubRepoName} for ${service.name}`)
-                await triggerWorkflow(service.name, service.branch)
+                await triggerWorkflow(service.id, service.branch)
                 return
             default:
                 console.log(`unhandled webhook type ${payload.type} for service ${payload.data.serviceId}`)
@@ -94,14 +115,14 @@ async function handleWebhook(payload: WebhookPayload) {
     }
 }
 
-async function triggerWorkflow(serviceName: string, branch: string) {
+async function triggerWorkflow(serviceID: string, branch: string) {
     await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
         owner: githubOwnerName,
         repo: githubRepoName,
         workflow_id: githubWorkflowID,
         ref: branch,
         inputs: {
-            // service: serviceName
+            serviceID: serviceID
         },
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
@@ -113,21 +134,23 @@ async function triggerWorkflow(serviceName: string, branch: string) {
 // some events have additional information that isn't in the webhook payload
 // for example, deploy events have the deploy id
 async function fetchEventInfo(payload: WebhookPayload): Promise<RenderEvent> {
+    const url = `${renderAPIURL}/events/${payload.data.id}`
+		console.log(`fetching event info at ${url}`)
     const res = await fetch(
-        `${renderAPIURL}/events/${payload.data.id}`,
+        url,
         {
-            method: "get",
+            method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                Authorization: `Bearer ${renderAPIToken}`,
+                accept: "application/json",
+                authorization: `Bearer ${renderAPIToken}`,
             },
         },
     )
+
     if (res.ok) {
         return res.json()
     } else {
-        throw new Error(`unable to fetch event info; received code :${res.status.toString()}`)
+        throw new Error(`unable to fetch event info; received code ${res.status.toString()}`)
     }
 }
 
